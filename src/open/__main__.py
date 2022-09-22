a8c131af2d7ff7f8d12657fcacd7057c19188cbb
bichwaa <ptonny13@gmail.com>
2022-09-22 09:29:27 +0300

    experimenting

---
+++ src/open/__main__.py
@@ -0,0 +1,15 @@
+from conf import _parse_project_db
+from commands_api import go_to
+
+def open_project(name:str)->str:
+    paths_dict = _parse_project_db()
+    for k,v in paths_dict.items():
+        if k == name:
+            print("the path: ",v)
+            return go_to(v)
+        else:
+            print(f"Can't find the path for project: {name}. \n Make sure you have added it to the registry(see help docs).")
+    
+
+
+open_project('drop')
--- src/open/commands_api.py
+++ src/open/commands_api.py
@@ -1,20 +1,24 @@
-import subprocess
-
-
-def go_to(path:str) -> None:
-    """takes in path and opens it in terminal using cd"""
-    return subprocess.Popen('cd', cwd=path)
-
-def open_in(ide:str)->None:
-    """Opens cwd in named ide"""
-    return subprocess.run([ide, "."])
+import os
+# import subprocess
 
 def open_in_code()->None:
     """Opens cwd in vs-code"""
-    return subprocess.run(['code .'])
+    return os.system()
+    # return subprocess.run('code .', shell=True)
+
+def go_to(path:str) -> None:
+    """takes in path and opens it in terminal using cd"""
+    print(f'cd {path}')
+    # return os.system(f'cd {path}')
+    # subprocess.run(f'cd {path}', shell=True)
+    open_in_code()
+
+def open_in(ide:str)->None:
+    """Opens cwd in named ide"""
+    return os.system(f"{ide} .")
 
 
 def start_server(commands:list[str])->None:
     """takes in a list of command strings required to start a project and runs them one by one"""
     for comm in commands:
-        subprocess.run([comm])
+        os.system([comm])
